39916cda5d3d06e4f247fd026f574945625921dd
Timothy D Meadows II <timothy@timothymeadows.com>
2018-03-05 19:34:08 -0800

    Add basic Promise suport.

--- storagepool.js
+++ storagepool.js
@@ -1,33 +1,27 @@
 var StoragePool = function (contianerName) {
-    var public = { IndexedDB: false, IDBTransaction: false };
-    var indexedDB = window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB;
-    var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction;
-    var version = public.version = 1.0;
+    if (!Promise)
+        throw "No Promise support, can't continue.";
 
-    if (!indexedDB) {
-        console.log("IndexedDB support not found!");
-        return public;
-    } else public.IndexedDB = true;
+    return new Promise(function (resolve, reject) {
+        var public = { IndexedDB: false, IDBTransaction: false };
+        var indexedDB = window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB;
+        var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction;
+        var version = public.version = 1.0;
 
-    if (!IDBTransaction) {
-        console.log("IDBTransaction support not found!");
-        return public;
-    } else public.IDBTransaction = true;
+        if (!indexedDB) {
+            reject(Error("IndexedDB support not found!"));
+            return;
+        } else public.IndexedDB = true;
 
-    var connection = indexedDB.open(contianerName, version), database;
-    connection.onsuccess = function (event) {
-        database = connection.result;
-    };
+        if (!IDBTransaction) {
+            reject(Error("IDBTransaction support not found!"));
+            return;
+        } else public.IDBTransaction = true;
 
-    if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && module.exports)
-            exports = module['exports'] = public;
-
-        exports.StoragePool = public;
-    } else {
-        if (typeof root !== 'undefined')
-            root.StoragePool = public;
-    }
-
-    return public;
+        var connection = indexedDB.open(contianerName, version), database;
+        connection.onsuccess = function (event) {
+            database = connection.result;
+            resolve(public);
+        };
+    });
 };
